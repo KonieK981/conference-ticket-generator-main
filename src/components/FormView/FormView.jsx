b848270f5e01163fc02d94873f82bad49ae400b9
Andres Torres <atorres981@gmail.com>
2025-06-04 00:10:38 +0500

    fix: adjust title formatting and update gap in form styles for better layout consistency

--- src/components/FormView/FormView.jsx
+++ src/components/FormView/FormView.jsx
@@ -37,10 +37,7 @@ function FormView() {
 
   return (
     <div>
-      <h1 className="title">
-        Your Journey to Coding <br />
-        Conf 2025 Starts Here!{" "}
-      </h1>
+      <h1 className="title">Your Journey to Coding Conf 2025 Starts Here! </h1>
       <h4 className="subtitle">
         Secure your spot at next year's biggest coding conference.
       </h4>
