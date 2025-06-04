f79c6f7650ebae6e68a560b784053076c449f95c
Andres Torres <atorres981@gmail.com>
2025-06-04 21:32:57 +0500

    feat: add meta description to index.html, create robots.txt

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -36,15 +36,15 @@ function Input({
         >
           <path
             stroke="#f57261"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z"
           />
           <path fill="#f57261" d="M8.004 10.462V7.596ZM8 5.57v-.042Z" />
           <path
             stroke="#f57261"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M8.004 10.462V7.596M8 5.569v-.042"
           />
         </svg>
